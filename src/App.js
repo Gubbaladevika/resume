2532e681caa5f9550c3293a6cbeb23db26f14c6f
Devika <gubbaladevika65@gmail.com>
2026-01-30 22:51:44 +0530

    file changed

--- src/App.js
+++ src/App.js
@@ -21,12 +21,10 @@ function App() {
 
       {/* Career Objective */}
       <h3>CAREER OBJECTIVE</h3>
-      <p>
-        MCA student seeking an entry-level opportunity to apply
+      <p>MCA student seeking an entry-level opportunity to apply
         basic knowledge of web development technologies and to
         grow as a software professional while contributing to
-        organizational goals.
-      </p>
+        organizational goals. </p>
 
       {/* Education */}
       <h3>EDUCATION</h3>
@@ -48,18 +46,16 @@ function App() {
       {/* Projects */}
       <h3>PROJECTS</h3>
       <p>
-        <b>Personal Resume Website</b>
-        • Developed a personal resume website using React
-        • Uploaded code to GitHub and deployed using Vercel
+        <b>Personal Resume Website</b><br />
+         Developed a personal resume website using React<br />
+         Uploaded code to GitHub and deployed using Vercel
       </p>
 
       {/* Certifications */}
       <h3>CERTIFICATIONS</h3>
       <p>Cisco Networking Academy- C Essentials</p>
       <p>Cisco Networking Academy- Python Essentials</p>
-      <p>
-         Currently learning Web Development and React
-      </p>
+      <p>Currently learning Web Development and React</p>
       {/* Languages known*/}
       <h3>LANGUAGES KNOWN</h3>
         <p>Telugu</p>
